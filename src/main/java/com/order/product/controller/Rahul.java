f0646aa6962232483cc251b1395c5479bcc316bb
rahulnayak01 <91556256+rahulnayak01@users.noreply.github.com>
2026-02-28 20:54:12 +0530

    Add greeting message for Rahul

--- src/main/java/com/order/product/controller/Rahul.java
+++ src/main/java/com/order/product/controller/Rahul.java
@@ -3,5 +3,6 @@ package com.order.product.controller;
 public class Rahul {
     public static void main(String[] args) {
         System.out.println("Hello, Rahul!");
+        System.out.println("run, Rahul!");
     }
 }
